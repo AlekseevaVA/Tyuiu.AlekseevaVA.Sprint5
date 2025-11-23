db3d3bfe09d601aa9481d3841fef129ddfa09d8e
AlekseevaVA <alekseevavria@mail.ru>
2025-11-23 18:04:20 +0500

    Task2

---
+++ Tyuiu.AlekseevaVA.Sprint5.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+using Tyuiu.AlekseevaVA.Sprint5.Task2.V11.Lib;
+
+
+namespace Tyuiu.AlekseevaVA.Sprint5.Task2.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string path = Path.GetTempFileName();
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            Assert.AreEqual(true, fileExists);
+
+        }
+    }
+}
