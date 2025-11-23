a216a4d767041d882107f4400fafcbc0309dab9b
AlekseevaVA <alekseevavria@mail.ru>
2025-11-23 14:37:12 +0500

    Add project files.

---
+++ Tyuiu.AlekseevaVA.Sprint5.Task0.V7.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.AlekseevaVA.Sprint5.Task0.V7.Lib
+{
+    public class DataService : ISprint5Task0V7
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.GetTempFileName();
+            x = 4;
+            double res = -Math.Pow(x, 3) + 4 * Math.Pow(x, 2) - (3.0 / 2.0 * 4);
+            res = Math.Round(res);
+            File.WriteAllText(path, Convert.ToString(res));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.AlekseevaVA.Sprint5.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+using Tyuiu.AlekseevaVA.Sprint5.Task0.V7.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint5.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            string direct = System.IO.Path.GetTempPath();
+
+            string path = Path.GetTempFileName();
+            string apath = Path.Combine(direct, path);
+
+            FileInfo fileInfo = new FileInfo(apath);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(true, fileExist);
+        }
+    }
+}
