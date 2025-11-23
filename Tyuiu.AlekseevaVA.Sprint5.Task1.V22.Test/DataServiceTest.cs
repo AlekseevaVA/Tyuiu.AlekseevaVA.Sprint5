54676accd7dc0ce81553abeb950c436dd2f734d3
AlekseevaVA <alekseevavria@mail.ru>
2025-11-23 16:57:46 +0500

    Task1

---
+++ Tyuiu.AlekseevaVA.Sprint5.Task1.V22.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+using Tyuiu.AlekseevaVA.Sprint5.Task1.V22.Lib;
+
+
+namespace Tyuiu.AlekseevaVA.Sprint5.Task1.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string path = Path.GetTempFileName();
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AlekseevaVA.Sprint5.Task1.V22/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.AlekseevaVA.Sprint5.Task1.V22.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint5.Task1.V22;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+     
+        int startValue = -5;
+        int stopValue = 5;
+
+        DataService ds = new DataService();
+        
+        Console.Title = "Спринт #5 | Выполнилa: Алексеева В. А. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнила: Алексеева В. А. | ИИПб-25-1                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Дано выражение. Вычислить его значение при x = 4, результат сохранить   *");
+        Console.WriteLine("* в текстовый файл 'OutPutFileTask0.txt' и вывести на консоль.            *");
+        Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+       
+
+        Console.WriteLine("startValue = "+ startValue);
+        Console.WriteLine("stopValue = "+ stopValue );
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Файл: " + ds.SaveToFileTextData(startValue, stopValue));
+        Console.WriteLine("Создан");
+        Console.ReadKey();
+    }
+}
