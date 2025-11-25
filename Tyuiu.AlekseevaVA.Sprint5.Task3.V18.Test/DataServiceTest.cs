c2021dace4c0e3a6258765e27bfb56e2042f89c6
AlekseevaVA <alekseevavria@mail.ru>
2025-11-25 13:21:16 +0500

    Task3

---
+++ Tyuiu.AlekseevaVA.Sprint5.Task3.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AlekseevaVA.Sprint5.Task3.V18.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint5.Task3.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string path = Path.GetTempFileName();
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AlekseevaVA.Sprint5.Task3.V18/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.AlekseevaVA.Sprint5.Task3.V18.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint5.Task3.V18;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        int x = 2;
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнилa: Алексеева В. А. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнила: Алексеева В. А. | ИИПб-25-1                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine("x = " + x);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine(" Создан!");
+        
+
+        Console.ReadKey();
+    }
+}
