ef5b7cb7178d9b04a7c9145e9b145a0ee3e7edd6
AlekseevaVA <alekseevavria@mail.ru>
2025-11-27 11:47:54 +0500

    Task6

---
+++ Tyuiu.AlekseevaVA.Sprint5.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+using Tyuiu.AlekseevaVA.Sprint5.Task6.V4.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint5.Task6.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\виктория\Desktop\DataSprint5\InPutDataFileTask6V4.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            Assert.AreEqual(true, fileExists);
+
+        }
+    }
+}
---
+++ Tyuiu.AlekseevaVA.Sprint5.Task6.V4/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AlekseevaVA.Sprint5.Task6.V4.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint5.Task6.V4;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнилa: Алексеева В. А. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Работа в С#                                                       *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #4                                                              *");
+        Console.WriteLine("* Выполнила: Алексеева В. А. | ИИПб-25-1                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\Users\виктория\Desktop\DataSprint5\InPutDataFileTask6V4.txt";
+
+        Console.WriteLine("Данные находятся в файле: " + path);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("Ответ " + res);
+
+        Console.ReadKey();
+    }
+}
